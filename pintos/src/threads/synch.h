5265bfcb37ca81a26de8f205ddcd2ee78ec6dab8
Isatherune <Isatherune@gmail.com>
2019-09-30 00:45:56 +0900

    Project 1 Priority Scheduling
    Implemented lock elem which have list element, lock and priority.(Moved form synch.c)

--- pintos/src/threads/synch.h
+++ pintos/src/threads/synch.h
@@ -22,7 +22,14 @@ struct lock
   {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
-    int priority; /* new */
+  };
+
+/* new */
+struct lock_elem 
+  {
+    struct list_elem elem; 
+    struct lock lock;
+    int priority;
   };
 
 void lock_init (struct lock *);
