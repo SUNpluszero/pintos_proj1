89c488e00d8f3d8ab161ed544c9b180432770894
Isatherune <Isatherune@gmail.com>
2019-09-30 00:39:05 +0900

    Project 1 Priority Scheduling
    Role back lock_init.
    Implemented lock_elem which have list element, lock, and priority.

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -200,7 +200,6 @@ lock_init (struct lock *lock)
 
   lock->holder = NULL;
   sema_init (&lock->semaphore, 1);
-  lock_priority = 0;
 }
 
 /* Acquires LOCK, sleeping until it becomes available if
@@ -273,6 +272,14 @@ struct semaphore_elem
   {
     struct list_elem elem;              /* List element. */
     struct semaphore semaphore;         /* This semaphore. */
+  };
+
+/* new */
+struct lock_elem 
+  {
+    struct list_elem elem; 
+    struct lock lock;
+    int priority;
   };
 
 /* Initializes condition variable COND.  A condition variable
