b22dec89df76c5af114d58c61885d77cbbf01bba
Isatherune <Isatherune@gmail.com>
2019-09-30 22:28:29 +0900

    Project 1 Priority Scheduling
    Roleback the code

--- pintos/src/threads/synch.h
+++ pintos/src/threads/synch.h
@@ -21,10 +21,10 @@ void sema_self_test (void);
 /* Lock. */
 struct lock 
   {
+    struct list_elem lock_elem;      /* Proj1 : - List element(for priority list). */
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
-    struct list_elem elem;
-    int max_priority;
+    int max_priority;           /* Proj1 : - Max priority of its semaphore threads*/
   };
 
 void lock_init (struct lock *);
