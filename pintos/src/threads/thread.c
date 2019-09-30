c65a78e3a39f207efdf36da873b11786a3b62d44
Isatherune <Isatherune@gmail.com>
2019-09-30 18:36:31 +0900

    Project 1 Priority Scheduling
    Modified set_priority to consider priority donation occurred case.

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -345,25 +345,37 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority) 
 {
-  int old_priority = thread_current()->priority;
-  int ori_priority = thread_current()->priority_original;
-  /*if current thread got donation*/
-  if(old_priority != ori_priority){
-    if(new_priority > old_priority){
-      thread_current()->priority = new_priority;
-      thread_current()->priority_original = new_priority;
-    }else{
-      thread_current()->priority_original = new_priority;
-    }
-  }else{
-    thread_current()->priority = new_priority;
-    /*proj1*/
-    thread_current()->priority_original = new_priority;
+  if (list_empty (&thread_current()->holding_locks))
+  {
+     thread_current()->priority = new_priority;
+     thread_current()->priority_original = new_priority;
+     if(new_priority < old_priority)
+       thread_yield();
   }
-  
-  /*if higher priority*/
-  if(new_priority > old_priority)
-    thread_yield();
+  else
+  {
+     if(thread_current()->priority_original != old_priority)
+     {
+        thread_current()->priority_original = new_priority;
+        if(new_priority > old_priority)
+           thread_current()->priority = new_priority;
+     }
+     else
+     {
+        thread_current()->priority_original = new_priority;
+        if(new_priority >= old_priority)
+           thread_current()->priority = new_priority;
+        else
+        {
+           donation_priority = list_entry(list_max(&thread_current()->holding_locks, compare_priority_of_lock, NULL), struct lock_elem, elem) -> priority
+           if(new_priority < donation_priority)
+              thread_current()->priority = donation_priority;
+           else
+              thread_current()->priority = new_priority;
+           thread_yield();
+        }
+     }
+  }
 }
 
 /* Returns the current thread's priority. */
